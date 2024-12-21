33ed776672e31ea9e2eafc1070f4ed95fcb3c2ef
agustinrodriguez7 <42780733+agustinrodriguez7@users.noreply.github.com>
2024-12-20 23:31:21 -0300

    Add comment in case of terraform installation error

--- install.sh
+++ install.sh
@@ -66,6 +66,7 @@ install_aws_cli () {
 }
 
 install_terraform () {
+  #If this is not working, try download the binary and add to the PATH. https://developer.hashicorp.com/terraform/install?product_intent=terraform
   wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg -y --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
   echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(. /etc/os-release && echo "$UBUNTU_CODENAME") main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
   sudo apt update && sudo apt install terraform
